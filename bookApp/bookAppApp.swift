c897f8dbad17e0f80ba7455df813cb1100d023f1
taokyone <antonsson.andreas@hotmail.com>
2023-09-28 18:17:26 +0200

    Initial Commit

---
+++ bookApp/bookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookAppApp.swift
+//  bookApp
+//
+//  Created by Andreas Antonsson on 2023-09-28.
+//
+
+import SwiftUI
+
+@main
+struct bookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
